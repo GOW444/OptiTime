3eff525a3cda9b9862a6787ab1d85c261d75bbae
Shivansh-Shah <shivanshshah317@gmail.com>
2025-11-23 11:59:06 +0530

    tried to implement this part in ReadMe

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python3 generate_data.py
-python3 preprocess.py
-# try MILP; if MILP fails, you can run greedy instead
-python3 model_core.py
-python3 validate.py
-echo "Done. Check results/assignments_milp.csv and results/metrics_milp.json"
